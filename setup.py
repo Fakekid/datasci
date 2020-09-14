ad63107f88623a895796a4ed69cc5bd14db45438
连晓磊 <lianxiaolei@tal.com>
2020-09-14 19:14:29 +0800

    update:添加eda model preprocessing路径

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 inner_packages = find_packages(include=['datasci.data_loader.*', 'datasci.eda.*',
                                         'datasci.model.*', 'datasci.preprocesing.*'])
 setup(name='datasci',
-      version='0.0.1',
+      version='0.0.2',
       description='TAL WangXiao Data Science Toolkit',
       url='',
       author='wangyue,lianxiaolei',
