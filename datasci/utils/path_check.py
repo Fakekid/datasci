e75e7b25c269fdab7c9b619c9a391a92f659e38c
zhaolihan <zhaolihan@tal.com>
2021-03-02 20:59:40 +0800

    FIX: 修复配置文件检测逻辑

--- datasci/utils/path_check.py
+++ datasci/utils/path_check.py
@@ -5,5 +5,6 @@ def check_path(path, is_make=True):
     if not os.path.exists(path=path):
         if is_make:
             os.mkdir(path=path)
+            raise Exception("Path %s is new created" % path)
         else:
             raise FileNotFoundError("Path %s is not Found" % path)
