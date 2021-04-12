937b169a3291c7c6e708ad8adc5767cbbce42abf
zhaolihan <zhaolihan@tal.com>
2021-04-12 19:02:29 +0800

    add evaluate func

--- datasci/evaluate/tal_evaluate.py
+++ datasci/evaluate/tal_evaluate.py
@@ -10,7 +10,7 @@ class CommonEvaluate(object):
         label = data['label']
         cls_data = data['cls']
         proba_data = data.drop(['label', 'cls'], axis=1)
-        metrics_dict['auc'] = metrics.roc_auc_score(label, proba_data[:, 1])
+        metrics_dict['auc'] = metrics.roc_auc_score(label, proba_data.iloc[:, 1])
         metrics_dict['recall'] = metrics.recall_score(label, cls_data)
         metrics_dict['precision'] = metrics.precision_score(label, cls_data)
         metrics_dict['f1_score'] = metrics.f1_score(label, cls_data)
